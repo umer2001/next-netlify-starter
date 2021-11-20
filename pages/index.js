936c49dbcdf6d047986d1b9d472d5ec9d56f4666
umer2001 <umer2001.uf@gmail.com>
2021-11-21 01:43:44 +0500

    testing: ststi props

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Footer from "@components/Footer";
 
-export default function Home() {
+export default function Home({ data: news }) {
   return (
     <div className="container">
       <Head>
@@ -11,7 +10,11 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        {new Array(5).fill(0).map((element, index) => (
+          <div key={index}>
+            <p className="description">{news[index].title}</p>
+          </div>
+        ))}
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
@@ -19,5 +22,21 @@ export default function Home() {
 
       <Footer />
     </div>
-  )
+  );
+}
+
+// This gets called on every request
+export async function getStaticProps() {
+  // Fetch data from external API
+  const res = await fetch(
+    `https://min-api.cryptocompare.com/data/v2/news/?lang=EN`,
+    {
+      headers: {
+        authorization: `Apikey ${process.env.CRYPTO_COMPARE_API_KEY}`,
+      },
+    }
+  );
+  var { Data: data } = await res.json();
+  // Pass data to the page via props
+  return { props: { data } };
 }
